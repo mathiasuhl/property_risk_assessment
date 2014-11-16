447ba4148cb81402a1b2f3e55a7ff5f00669de0b
Prelang Builder <builder@prelang.com>
2014-11-16 19:58:45 +0000

    Generated model Tanant with migration

---
+++ app/models/tanant.rb
@@ -0,0 +1,2 @@
+class Tanant < ActiveRecord::Base
+end
---
+++ db/migrate/20141116195845_create_tanants.rb
@@ -0,0 +1,12 @@
+class CreateTanants < ActiveRecord::Migration
+  def change
+    create_table :tanants do |t|
+      t.string :company
+      t.string :location
+      t.string :address
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
