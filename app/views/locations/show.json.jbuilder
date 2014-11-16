7fd84dccebe0960dc28a401c425743de8bbe70d6
Prelang Builder <builder@prelang.com>
2014-11-16 19:59:05 +0000

    Generated scaffolding for model 'Location' via Rails

---
+++ app/views/locations/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @location, :id, :name, :property_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :locations
+
   resources :services
 
   resources :insurance_facilities
