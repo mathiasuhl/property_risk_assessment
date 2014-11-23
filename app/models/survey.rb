66997c4ee2fcec66445c044027408a8b2749b536
mathias uhl <mathiasuhl@gmx.de>
2014-11-23 12:32:56 -0500

    v2

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,5 @@
 class Survey < ActiveRecord::Base
   belongs_to :property
 
-  enum :cracks ['TG','Aussenwände', 'Innenwände','Decken' ]
-  enum :damp ['TG','Aussenwände', 'Innenwände','Decken' ]
+       
 end
